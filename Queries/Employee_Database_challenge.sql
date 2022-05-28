f0f5134224eded93fcce0c2ccae62202ea1cd528
Zach Stasz <zachstasz@gmail.com>
2022-05-28 12:55:46 -0400

    Deliverable 1 & 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,40 @@
+-- Deliverable 1
+
+-- Retirement Titles
+SELECT e.emp_no,e.first_name,e.last_name,t.title,t.from_date,t.to_date
+INTO retirement_titles
+FROM employees AS e
+JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name,last_name,title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+-- Number of Retirement Titles
+SELECT COUNT(emp_no), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+
+-- Deliverable 2
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no, e.first_name,e.last_name,e.birth_date, de.from_date, de.to_date,t.title
+INTO mentorship
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+ON (de.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship;
